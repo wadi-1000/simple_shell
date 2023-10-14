2258e20d80faceeaf11ec79fcd04de08fc33414f
Joy Kiranga <joykiranga@gmail.com>
2023-10-14 10:00:55 +0300

    Removed the execute funtion

--- prompt.c
+++ prompt.c
@@ -46,39 +46,3 @@ void prompt(void)
 	}
 	free(buffer);
 }
-
-/**
- * exec_input - Execute the command entered by the user.
- * @argv: Array of command arguments.
- *
- *
- * Return: exits the process upon success or failure.
- */
-void exec_input(char *argv[])
-{
-	pid_t pid;
-	int status;
-
-	pid = fork();
-
-	if (pid < 0)
-	{
-		perror("Fork failed.");
-		exit(1);
-	}
-
-	if (pid == 0)
-	{
-		if (execvp(argv[0], argv) < 0)
-		{
-			fprintf(stderr, "Execution failed. Incorrect command
-					or no such file/directory.\n");
-			return;
-		}
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-	}
-	exit(0);
-}
